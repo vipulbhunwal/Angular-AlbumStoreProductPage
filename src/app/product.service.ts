e7c2e837ef31f3a7d7366c75ab2765f7bb499135
vipulbhunwal <vipulbhunwal@gmail.com>
2019-04-16 17:48:18 +0530

    updating part 7

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,14 +4,20 @@ import { getLocaleNumberSymbol } from '@angular/common';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import {Album} from './album';
+import {Product} from './product';
 @Injectable()
 export class ProductService {
-  private _albumUrl = '../assets/album.json'
+  private _albumUrl = '../assets/album.json';
+  private _productsUrl ='../assets/products.json';
   constructor(private _http: Http) {
     
   }
   getAlbum(id:number): Observable<Album>{
     return this._http.get(this._albumUrl).map((Response)=><Album>Response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
